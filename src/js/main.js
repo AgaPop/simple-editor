1f6cb0872257658f603869c188f2416167ac9be4
Aga Popko <aga.majchrzak7@gmail.com>
2019-07-16 01:07:51 +0200

    feat: editor - first version

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
-"use strict";
+// zapamietywanie danych
+const name = document.querySelector('.form__input--name--js');
+const surname = document.querySelector('.form__input--surname--js');
+const description = document.querySelector('.form__textarea--js');
 
-import moment from 'moment';
+const buttonLoad = document.querySelector('.form__button--load--js');
+const buttonSave = document.querySelector('.form__button--save--js');
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+buttonSave.addEventListener('click', (e) => {
+    e.preventDefault(); /*blokuje domyslne wysylanie formularza*/
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('surname', surname.value);
+    localStorage.setItem('description', description.value);   
+});
+
+
+buttonLoad.addEventListener('click', (e) => {
+    e.preventDefault();    
+    description.value = localStorage.getItem('description');  
+    surname.value = localStorage.getItem('surname'); 
+    name.value = localStorage.getItem('name');                    
+});
